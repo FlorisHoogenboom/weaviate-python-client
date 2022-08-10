75003772f15332af6d9ee4fddcf13da01eb4c2f9
Stefan Bogdan <stefanbogdan180394@gmail.com>
2022-08-10 14:56:32 +0200

    add unittests for weaviate.util

--- weaviate/util.py
+++ weaviate/util.py
@@ -26,27 +26,20 @@ def image_encoder_b64(image_file_or_path: Union[str, BufferedReader]) -> str:
 
     Raises
     ------
-    ValueError
-        If the argument is str and does not point to an existing file.
     TypeError
         If the argument is of a wrong data type.
     """
 
 
     if isinstance(image_file_or_path, str):
-        if not os.path.isfile(image_file_or_path):
-            raise ValueError(
-                f"No file found at location: '{image_file_or_path}'."
-            )
         with open(image_file_or_path, 'br') as file:
             content = file.read()
-
     elif isinstance(image_file_or_path, BufferedReader):
         content = image_file_or_path.read()
     else:
         raise TypeError(
             "'image_or_image_path' should be an image path or a binary-read file "
-            "io.BufferedReader."
+            "io.BufferedReader"
         )
     return base64.b64encode(content).decode("utf-8")
 
@@ -95,7 +88,7 @@ def generate_local_beacon(uuid: Union[str, uuid_lib.UUID], class_name: str) -> d
 
     if not isinstance(class_name, str):
         raise TypeError(
-            f"'class_name' must be of type str. Given type: {type(class_name)}."
+            f"'class_name' must be of type str. Given type: {type(class_name)}"
         )
 
     _uuid = get_valid_uuid(uuid=uuid)
@@ -129,20 +122,19 @@ def get_valid_uuid(uuid: Union[str, uuid_lib.UUID]) -> str:
     """
 
     if isinstance(uuid, uuid_lib.UUID):
-        return str(uuid)
+        return uuid.hex
 
     if not isinstance(uuid, str):
         raise TypeError(
-            f"'uuid' must be of type str or uuid.UUID. Given type: {type(uuid)}."
+            f"'uuid' must be of type str or uuid.UUID. Given type: {type(uuid)}"
         )
 
     try:
-        _uuid = str(uuid_lib.UUID(uuid))
+        return uuid_lib.UUID(uuid).hex
     except ValueError:
         raise ValueError(
-            "Not valid 'uuid' or 'uuid' can not be extracted from value."
+            "Not valid 'uuid' or 'uuid' can not be extracted from value"
         ) from None
-    return _uuid
 
 
 def get_vector(vector: Sequence[Real]) -> List[Real]:
@@ -179,7 +171,7 @@ def get_vector(vector: Sequence[Real]) -> List[Real]:
         except AttributeError:
             raise TypeError(
                 "The type of the 'vector' argument is not supported. "
-                "Supported types are 'list', 'numpy.ndarray', 'torch.Tensor' and 'tf.Tensor'."
+                "Supported types are 'list', 'numpy.ndarray', 'torch.Tensor' and 'tf.Tensor'"
             ) from None
 
 
@@ -201,7 +193,7 @@ def generate_uuid5(identifier: Any, namespace: Any = "") -> str:
         The UUID as a string.
     """
 
-    return str(uuid_lib.uuid5(uuid_lib.NAMESPACE_DNS, str(namespace) + str(identifier)))
+    return str(uuid_lib.uuid5(uuid_lib.NAMESPACE_DNS, str(identifier) + str(namespace)))
 
 
 def capitalize_first_letter(string: str) -> str:
